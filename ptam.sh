77823f94c2449ef30097e7dc780a9d46884ddc35
ddetone <daniel.detone@gmail.com>
2013-09-23 03:27:03 +0000

    added last package

--- ptam.sh
+++ ptam.sh
@@ -9,16 +9,24 @@ sudo apt-get install build-essential
 sudo apt-get install -y libblas-dev liblapack-dev libfortran3 ncurses-dev libreadline-dev libdc1394-22-dev libtiff-dev libjpeg-dev libpng-dev
 
 
-
+#--- TooN installation
 cd TooN/
 ./configure
 make
 sudo make install
 cd ..
 
+#--- libcvd installation
 export CXXFLAGS=-D_REENTRANT
 cd libcvd/
 ./configure --without-ffmpeg
+make
+sudo make install
+cd ..
+
+#--- gvars installation
+cd gvars/
+./configure --disable-widgets
 make
 sudo make install
 cd ..
