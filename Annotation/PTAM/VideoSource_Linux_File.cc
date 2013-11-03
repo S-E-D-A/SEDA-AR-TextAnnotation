17c54522cf2a316b4db08b7bcadc767f30e67b39
ddetone <daniel.detone@gmail.com>
2013-11-03 17:19:12 +0000

    fix for linker error from videosource from file

--- Annotation/PTAM/VideoSource_Linux_File.cc
+++ Annotation/PTAM/VideoSource_Linux_File.cc
@@ -1,8 +1,8 @@
 // Copyright 2008 Isis Innovation Limited
 #include "VideoSource.h"
 #include <cvd/videofilebuffer.h>
-#include <cvd/colourspace_convert.h>
-#include <cvd/colourspaces.h>
+#include <cvd/videodisplay.h>
+#include <cvd/gl_helpers.h>
 #include <gvars3/instances.h>
 
 using namespace CVD;
@@ -12,12 +12,7 @@ using namespace GVars3;
 VideoSource::VideoSource() 
 {
 cout << "  VideoSource_Linux: Opening video source..." << endl;
-// string QuickCamFile = GV3::get<string>("VideoSource.V4LDevice", "/dev/video0");
-// ImageRef irSize = GV3::get<ImageRef>("VideoSource.Resolution", ImageRef(640,480));
-// int nFrameRate = GV3::get<int>("VideoSource.Framerate", 30);
-// V4LBuffer<yuv422>* pvb = new V4LBuffer<yuv422>(QuickCamFile, irSize, -1, false, nFrameRate);
-
-string VideoFile = "file:///home/vagrant/SEDA-CV-DetectionAndAnnotation/Annotation/TestLibCVD/iphone_001.m4v";
+string VideoFile = "file:///home/vagrant/SEDA-CV-DetectionAndAnnotation/Annotation/PTAM/iphone_001.m4v";
 VideoFileBuffer<Rgb<byte> > * pvb = new VideoFileBuffer<Rgb<byte> >(VideoFile);
 mirSize = pvb->size();
 mptr = pvb;
