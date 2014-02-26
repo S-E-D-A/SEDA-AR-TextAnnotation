318709fb47ed4a39397701315366bc507cff11f7
ddetone <daniel.detone@gmail.com>
2014-02-25 23:19:41 -0500

    render text at each canvas

--- TextGame.cc
+++ TextGame.cc
@@ -15,11 +15,43 @@ void TextGame::Draw()
     if (!mbInitialized)
         Init();
 
+    //Lighting Effects
+    glFrontFace(GL_CW);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_COLOR_MATERIAL);
+    GLfloat af[4];
+    af[0]=0.5;
+    af[1]=0.5;
+    af[2]=0.5;
+    af[3]=1.0;
+    glLightfv(GL_LIGHT0, GL_AMBIENT, af);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, af);
+    af[0]=1.0;
+    af[1]=0.0;
+    af[2]=1.0;
+    af[3]=0.0;
+    glLightfv(GL_LIGHT0, GL_POSITION, af);
+    af[0]=1.0;
+    af[1]=1.0;
+    af[2]=1.0;
+    af[3]=1.0;
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, af);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 50.0);
+
+
     glMatrixMode(GL_MODELVIEW);
-    glColor3f(0.15f, 0.15f, 0.15f); //Grey
+    glColor3f(0.85f, 0.85f, 0.85f); //Grey
     glLoadIdentity();
+    glMultMatrix(mse3CanvasFromWorld.inverse());
     glScaled(mdScale, mdScale, 0.0f);
     glCallList(mnTextDisplayList);
+
+    glDisable(GL_LIGHTING);
+    glDisable(GL_DEPTH_TEST);
 
 }
 
